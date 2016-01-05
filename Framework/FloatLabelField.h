9fd4c1370692d6ae84ba3c2bb967d7d166ed7b89
Katsuma Ito <katsumai@gmail.com>
2016-01-05 17:01:32 +0900

    Add framework project

---
+++ Framework/FloatLabelField.h
@@ -0,0 +1,25 @@
+//
+//  FloatLabelField.h
+//  FloatLabelField
+
+//
+//  Created by Fahim Farook on 28/11/14.
+//  Copyright (c) 2014 RookSoft Ltd. All rights reserved.
+//
+//  Original Concept by Matt D. Smith
+//  http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
+//
+//  Objective-C version by Jared Verdi
+//  https://github.com/jverdi/JVFloatLabeledTextField
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatLabelField.
+FOUNDATION_EXPORT double FloatLabelFieldVersionNumber;
+
+//! Project version string for FloatLabelField.
+FOUNDATION_EXPORT const unsigned char FloatLabelFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatLabelField/PublicHeader.h>
+
+
